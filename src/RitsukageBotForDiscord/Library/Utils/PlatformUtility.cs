e91eafa7c64a35c46ec5570365336e5e0d41a398
OLC <utsumabo@ritsukage.com>
2025-01-16 14:31:17 +0800

    update os checker

--- src/RitsukageBotForDiscord/Library/Utils/PlatformUtility.cs
+++ src/RitsukageBotForDiscord/Library/Utils/PlatformUtility.cs
@@ -5,12 +5,19 @@ namespace RitsukageBot.Library.Utils
     /// </summary>
     public static class PlatformUtility
     {
+        private static PlatformID OperatingSystem { get; } = InnerGetOperatingSystem();
+
         /// <summary>
         ///     Get the operating system.
         /// </summary>
         /// <returns></returns>
         /// <exception cref="PlatformNotSupportedException"></exception>
         public static PlatformID GetOperatingSystem()
+        {
+            return OperatingSystem;
+        }
+
+        private static PlatformID InnerGetOperatingSystem()
         {
             var winDir = Environment.GetEnvironmentVariable("windir");
             if (!string.IsNullOrEmpty(winDir) && winDir.Contains('\\') && Directory.Exists(winDir))
@@ -27,6 +34,33 @@ namespace RitsukageBot.Library.Utils
                 return PlatformID.MacOSX;
 
             throw new PlatformNotSupportedException("Unknown OS type.");
+        }
+
+        /// <summary>
+        ///     Check if the operating system is Windows.
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsWindows()
+        {
+            return OperatingSystem == PlatformID.Win32NT;
+        }
+
+        /// <summary>
+        ///     Check if the operating system is Linux.
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsLinux()
+        {
+            return OperatingSystem == PlatformID.Unix;
+        }
+
+        /// <summary>
+        ///     Check if the operating system is macOS.
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsMacOs()
+        {
+            return OperatingSystem == PlatformID.MacOSX;
         }
     }
 }
