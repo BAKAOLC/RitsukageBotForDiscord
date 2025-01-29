c19a98607c2c1f7a52d20a0ee8c736b8a6841064
OLC <utsumabo@ritsukage.com>
2025-01-29 15:36:18 +0800

    modify ai output

--- src/RitsukageBotForDiscord/Modules/AiInteractions.cs
+++ src/RitsukageBotForDiscord/Modules/AiInteractions.cs
@@ -1,5 +1,4 @@
 using System.Text;
-using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
 using Microsoft.Extensions.AI;
@@ -42,7 +41,7 @@ namespace RitsukageBot.Modules
         [SlashCommand("chat", "Chat with the AI")]
         public async Task ChatAsync(string message)
         {
-            await DeferAsync(true).ConfigureAwait(false);
+            await DeferAsync().ConfigureAwait(false);
             if (string.IsNullOrWhiteSpace(message))
                 await ModifyOriginalResponseAsync(x => x.Content = "Please provide a message to chat with the AI")
                     .ConfigureAwait(false);
@@ -55,29 +54,6 @@ namespace RitsukageBot.Modules
                 messageList.Insert(0, roleData);
             await BeginChatAsync(messageList).ConfigureAwait(false);
         }
-
-        /*
-        /// <summary>
-        ///     Chat with the AI using AI tools
-        /// </summary>
-        [RequireOwner]
-        [SlashCommand("chat-tools", "Chat with the AI using AI tools")]
-        public async Task ChatWithAiToolsAsync(string message)
-        {
-            await DeferAsync(true).ConfigureAwait(false);
-            if (string.IsNullOrWhiteSpace(message))
-                await ModifyOriginalResponseAsync(x => x.Content = "Please provide a message to chat with the AI")
-                    .ConfigureAwait(false);
-
-            var messageList = new List<ChatMessage>
-            {
-                new(ChatRole.User, message),
-            };
-           if (GetRoleData() is { } roleData)
-               messageList.Insert(0, roleData);
-            await BeginChatAsync(messageList, true).ConfigureAwait(false);
-        }
-        */
 
         private ChatMessage? GetRoleData()
         {
@@ -101,10 +77,8 @@ namespace RitsukageBot.Modules
             }
 
             var sb = new StringBuilder();
-            AppendUserMessage(sb, userInputMessage);
             var isCompleted = false;
             var isUpdated = false;
-            var isErrored = false;
             var haveContent = false;
             var lockObject = new Lock();
             _ = Task.Run(async () =>
@@ -123,9 +97,7 @@ namespace RitsukageBot.Modules
                 if (!x.IsFaulted) return;
                 isCompleted = true;
                 isUpdated = true;
-                isErrored = true;
                 sb = new();
-                AppendUserMessage(sb, userInputMessage);
                 sb.Append("An error occurred while processing the chat with AI tools");
                 Logger.LogError(x.Exception, "Error while processing the chat with AI tools");
             });
@@ -151,78 +123,6 @@ namespace RitsukageBot.Modules
 
             if (!haveContent)
                 await ModifyOriginalResponseAsync(x => x.Content = "No response from the AI").ConfigureAwait(false);
-
-            if (!isErrored && haveContent)
-            {
-                var components = new ComponentBuilder();
-                components.WithButton("Publish", $"{CustomId}:publish");
-                await ModifyOriginalResponseAsync(x => x.Components = components.Build()).ConfigureAwait(false);
-            }
-        }
-
-        private static StringBuilder AppendUserMessage(StringBuilder sb, string message)
-        {
-            var lines = message.Split('\n');
-            foreach (var line in lines)
-            {
-                sb.Append("> ");
-                sb.AppendLine(line);
-            }
-
-            sb.AppendLine();
-            return sb;
-        }
-    }
-
-
-    /// <summary>
-    ///     Interaction button for AI
-    /// </summary>
-    public class AiInteractionButton : InteractionModuleBase<SocketInteractionContext<SocketMessageComponent>>
-    {
-        /// <summary>
-        ///     Logger
-        /// </summary>
-        public required ILogger<AiInteractionButton> Logger { get; set; }
-
-        /// <summary>
-        ///     Publish the chat with AI
-        /// </summary>
-        [ComponentInteraction($"{AiInteractions.CustomId}:publish")]
-        public async Task PublishAsync()
-        {
-            await Context.Interaction.UpdateAsync(x => x.Components = null).ConfigureAwait(false);
-            var embed = new EmbedBuilder();
-            var (userMessage, aiMessage) = SplitContent(Context.Interaction.Message.Content);
-            embed.AddField("User Message", userMessage);
-            embed.AddField("AI Message", aiMessage);
-            embed.WithTimestamp(Context.Interaction.Message.Timestamp);
-            embed.WithAuthor(Context.User);
-            embed.WithFooter("Ritsukage Bot", Context.Client.CurrentUser.GetAvatarUrl());
-            await Context.Channel.SendMessageAsync(embed: embed.Build()).ConfigureAwait(false);
-        }
-
-        private (string, string) SplitContent(string content)
-        {
-            var lines = content.Split('\n');
-            var userMessage = new StringBuilder();
-            var aiMessage = new StringBuilder();
-            var isUserMessage = true;
-            foreach (var line in lines)
-                if (line.StartsWith("> "))
-                {
-                    if (isUserMessage)
-                        userMessage.AppendLine(line[2..]);
-                    else
-                        aiMessage.AppendLine(line[2..]);
-                }
-                else
-                {
-                    isUserMessage = false;
-                    aiMessage.AppendLine(line);
-                }
-
-            return (userMessage.ToString(), aiMessage.ToString());
         }
     }
 }
