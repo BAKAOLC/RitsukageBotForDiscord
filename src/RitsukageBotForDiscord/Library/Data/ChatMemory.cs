8cd9363f1bb159aa707431583411e6ec9fe00e85
OLC <utsumabo@ritsukage.com>
2025-02-09 03:27:28 +0800

    AI update v3

---
+++ src/RitsukageBotForDiscord/Library/Data/ChatMemory.cs
@@ -0,0 +1,58 @@
+﻿using SQLite;
+
+namespace RitsukageBot.Library.Data
+{
+    /// <summary>
+    ///     Chat Memory
+    /// </summary>
+    [Table("ChatMemory")]
+    public class ChatMemory
+    {
+        /// <summary>
+        ///     Key.
+        /// </summary>
+        [PrimaryKey]
+        [Column("id")]
+        public string Key { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     Memory for user.
+        /// </summary>
+        [Column("user_id")]
+        public ulong UserId { get; set; }
+
+        /// <summary>
+        ///     Type.
+        /// </summary>
+        [Column("type")]
+        public ChatMemoryType Type { get; set; } = ChatMemoryType.ShortTerm;
+
+        /// <summary>
+        ///     Value.
+        /// </summary>
+        [Column("value")]
+        public string Value { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     Timestamp.
+        /// </summary>
+        [Column("timestamp")]
+        public DateTimeOffset Timestamp { get; set; } = DateTimeOffset.MinValue;
+    }
+
+    /// <summary>
+    ///     Chat Memory Type
+    /// </summary>
+    public enum ChatMemoryType
+    {
+        /// <summary>
+        ///     Short Term
+        /// </summary>
+        ShortTerm,
+
+        /// <summary>
+        ///     Long Term
+        /// </summary>
+        LongTerm,
+    }
+}
--- src/RitsukageBotForDiscord/Services/Providers/ChatClientProviderService.cs
+++ src/RitsukageBotForDiscord/Services/Providers/ChatClientProviderService.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using RitsukageBot.Library.Data;
 using ChatClientBuilder = Microsoft.Extensions.AI.ChatClientBuilder;
 using IChatClient = Microsoft.Extensions.AI.IChatClient;
 using OllamaChatClient = Microsoft.Extensions.AI.OllamaChatClient;
@@ -19,6 +21,7 @@ namespace RitsukageBot.Services.Providers
     {
         private readonly List<IChatClient> _chatClients = [];
         private readonly IConfiguration _configuration;
+        private readonly DatabaseProviderService _databaseProviderService;
         private readonly bool _isEnabled;
         private readonly ILogger<ChatClientProviderService> _logger;
 
@@ -30,6 +33,7 @@ namespace RitsukageBot.Services.Providers
         {
             _configuration = serviceProvider.GetRequiredService<IConfiguration>();
             _logger = serviceProvider.GetRequiredService<ILogger<ChatClientProviderService>>();
+            _databaseProviderService = serviceProvider.GetRequiredService<DatabaseProviderService>();
             var configuration = serviceProvider.GetRequiredService<IConfiguration>();
             _isEnabled = configuration.GetValue<bool>("AI:Enabled");
             if (!_isEnabled)
@@ -118,6 +122,51 @@ namespace RitsukageBot.Services.Providers
         }
 
         /// <summary>
+        ///     Get prompt extensions
+        /// </summary>
+        /// <returns></returns>
+        public string GetPromptExtensions()
+        {
+            var extensions = _configuration.GetSection("AI:PromptExtension").Get<PromptExtensionConfig[]>();
+            if (extensions is null || extensions.Length == 0)
+            {
+                _logger.LogWarning("Prompt extensions not found");
+                return string.Empty;
+            }
+
+            var sb = new StringBuilder();
+            foreach (var extension in extensions)
+            {
+                var prompt = extension.Prompt;
+                if (string.IsNullOrWhiteSpace(prompt))
+                {
+                    if (string.IsNullOrWhiteSpace(extension.PromptFile))
+                    {
+                        _logger.LogWarning("Prompt extension has no prompt or prompt file");
+                        continue;
+                    }
+
+                    if (!File.Exists(extension.PromptFile))
+                    {
+                        _logger.LogWarning("Prompt extension prompt file does not exist");
+                        continue;
+                    }
+
+                    prompt = File.ReadAllText(extension.PromptFile);
+                    if (string.IsNullOrWhiteSpace(prompt))
+                    {
+                        _logger.LogWarning("Prompt extension prompt is empty");
+                        continue;
+                    }
+                }
+
+                sb.AppendLine(prompt).AppendLine();
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        /// <summary>
         ///     Get role data
         /// </summary>
         /// <returns></returns>
@@ -150,13 +199,14 @@ namespace RitsukageBot.Services.Providers
                 }
 
                 prompt = File.ReadAllText(roleData.PromptFile);
+                if (string.IsNullOrWhiteSpace(prompt))
+                {
+                    _logger.LogWarning("Role {Type} prompt is empty", type);
+                    return false;
+                }
             }
 
-            if (string.IsNullOrWhiteSpace(prompt))
-            {
-                _logger.LogWarning("Role {Type} prompt is empty", type);
-                return false;
-            }
+            prompt = new StringBuilder(prompt).AppendLine().Append(GetPromptExtensions()).ToString().Trim();
 
             var chatRoleString = _configuration.GetValue<string>("AI:PromptRole", "System");
             var chatRole = chatRoleString.ToLower() switch
@@ -183,6 +233,137 @@ namespace RitsukageBot.Services.Providers
         }
 
         /// <summary>
+        ///     Get memory
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public async Task<JObject> GetMemory(ulong userId, ChatMemoryType type = ChatMemoryType.ShortTerm)
+        {
+            var table = _databaseProviderService.Table<ChatMemory>();
+            var memory = await table.Where(x => x.UserId == userId && x.Type == type)
+                .OrderBy(x => x.Timestamp)
+                .ToArrayAsync().ConfigureAwait(false);
+
+            if (memory.Length == 0) return new();
+
+            var data = new JObject();
+            foreach (var item in memory)
+                if (data.TryGetValue(item.Key, out var value))
+                {
+                    if (value is JArray { Count: > 0 } array)
+                        array.Add(item.Value);
+                    else
+                        data[item.Key] = new JArray { value, item.Value };
+                }
+                else
+                {
+                    data[item.Key] = item.Value;
+                }
+
+            return data;
+        }
+
+        /// <summary>
+        ///     Insert memory
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="type"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public async Task InsertMemory(ulong userId, ChatMemoryType type, string key, string value)
+        {
+            var memory = new ChatMemory
+            {
+                UserId = userId,
+                Type = type,
+                Key = key,
+                Value = value,
+                Timestamp = DateTimeOffset.Now,
+            };
+            await _databaseProviderService.InsertOrUpdateAsync(memory).ConfigureAwait(false);
+            _logger.LogInformation("Added new {Type} memory for {UserId} with key {Key}: {Value}", type, userId, key,
+                value);
+        }
+
+        /// <summary>
+        ///     Remove memory
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="type"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public async Task RemoveMemory(ulong userId, ChatMemoryType type, string key)
+        {
+            var table = _databaseProviderService.Table<ChatMemory>();
+            var memory = await table.Where(x => x.UserId == userId && x.Type == type && x.Key == key)
+                .FirstOrDefaultAsync()
+                .ConfigureAwait(false);
+            if (memory is not null)
+            {
+                await _databaseProviderService.DeleteAsync(memory).ConfigureAwait(false);
+                _logger.LogInformation("Removed {Type} memory for {UserId} with key {Key}: {Value}", type, userId, key,
+                    memory.Value);
+            }
+        }
+
+        /// <summary>
+        ///     Refresh short memory
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public async Task RefreshShortMemory(ulong userId, int length = 2000)
+        {
+            var table = _databaseProviderService.Table<ChatMemory>();
+            var memoryList = await table.Where(x => x.UserId == userId && x.Type == ChatMemoryType.ShortTerm)
+                .OrderByDescending(x => x.Timestamp)
+                .ToListAsync().ConfigureAwait(false);
+            var totalLength = memoryList.Sum(x => x.Value.Length);
+            while (totalLength > length)
+            {
+                var memory = memoryList[^1];
+                memoryList.RemoveAt(memoryList.Count - 1);
+                totalLength -= memory.Value.Length;
+                await _databaseProviderService.DeleteAsync(memory).ConfigureAwait(false);
+                _logger.LogInformation("Removed {Type} memory for {UserId} with key {Key}: {Value}",
+                    ChatMemoryType.ShortTerm,
+                    userId, memory.Key, memory.Value);
+            }
+        }
+
+        /// <summary>
+        ///     Build user chat message
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="id"></param>
+        /// <param name="time"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task<ChatMessage?> BuildUserChatMessage(string name, ulong id, DateTimeOffset time,
+            string message)
+        {
+            var (_, userInfo) = await _databaseProviderService.GetOrCreateAsync<ChatUserInformation>(id)
+                .ConfigureAwait(false);
+            var shortMemory = await GetMemory(id).ConfigureAwait(false);
+            var longMemory = await GetMemory(id, ChatMemoryType.LongTerm).ConfigureAwait(false);
+            var jObject = new JObject
+            {
+                ["name"] = name,
+                ["message"] = message,
+                ["data"] = new JObject
+                {
+                    ["short_memory"] = shortMemory,
+                    ["long_memory"] = longMemory,
+                    ["good"] = userInfo.Good,
+                    ["time"] = time.ToString("yyyy-MM-dd HH:mm:ss zzz"),
+                },
+            };
+            return new(ChatRole.User, jObject.ToString());
+        }
+
+        /// <summary>
         ///     Check message header
         /// </summary>
         /// <param name="response"></param>
@@ -193,35 +374,37 @@ namespace RitsukageBot.Services.Providers
             [NotNullWhen(true)] out string? jsonHeader)
         {
             response = response.Trim();
-            if (response is not ['{', ..])
+            if (response is not ['{', ..] or ['[', ..])
             {
                 content = response;
                 jsonHeader = null;
                 return false;
             }
 
-            var depth = 0;
+            var depth = new Stack<char>();
             var jsonStringBuilder = new StringBuilder();
             foreach (var c in response)
             {
-                if (c == '{')
+                switch (c)
                 {
-                    depth++;
-                }
-                else if (c == '}')
-                {
-                    depth--;
-                    if (depth == 0)
-                    {
-                        jsonStringBuilder.Append(c);
+                    case '{' or '[':
+                        depth.Push(c);
                         break;
-                    }
+                    case '}' or ']' when depth.Count == 0:
+                        content = response;
+                        jsonHeader = null;
+                        return false;
+                    case '}' or ']':
+                        depth.Pop();
+                        break;
                 }
 
                 jsonStringBuilder.Append(c);
+                if (depth.Count == 0)
+                    break;
             }
 
-            if (depth != 0)
+            if (depth.Count != 0)
             {
                 content = string.Empty;
                 jsonHeader = null;
@@ -288,6 +471,8 @@ namespace RitsukageBot.Services.Providers
         }
 
         internal record EndpointConfig(string Endpoint, string ApiKey, string ModelId);
+
+        internal record PromptExtensionConfig(string Prompt, string PromptFile);
 
         internal record RoleConfig(string Prompt, string PromptFile, float Temperature);
     }
