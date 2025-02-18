96f045846f9af33fa2282d35b355255818a3ab7c
OLC <utsumabo@ritsukage.com>
2025-02-18 13:31:38 +0800

    Revert "add embed"
    This reverts commit 45bbceb0fa96f03a6b78b328eb21ab0aba76dcdd.

--- src/RitsukageBotForDiscord/Modules/ImageInteractions.cs
+++ src/RitsukageBotForDiscord/Modules/ImageInteractions.cs
@@ -621,10 +621,7 @@ namespace RitsukageBot.Modules
             imageStream.Seek(0, SeekOrigin.Begin);
             image.Dispose();
             await ModifyOriginalResponseAsync(x =>
-                {
-                    x.Attachments = new List<FileAttachment> { new(imageStream, fileName) };
-                    x.Embed = new EmbedBuilder().WithColor(Color.Green).WithDescription("success").Build();
-                })
+                    x.Attachments = new List<FileAttachment> { new(imageStream, fileName) })
                 .ConfigureAwait(false);
             await imageStream.DisposeAsync().ConfigureAwait(false);
         }
@@ -636,10 +633,6 @@ namespace RitsukageBot.Modules
 
         private async Task TriggerProcessAsync<T>(T processStep) where T : IProcessStep<Rgba32>
         {
-            await ModifyOriginalResponseAsync(x =>
-            {
-                x.Embed = new EmbedBuilder().WithColor(Color.Orange).WithDescription("Processing...").Build();
-            }).ConfigureAwait(false);
             using var image = await GetImageAsync().ConfigureAwait(false);
             if (image is null) return;
 
