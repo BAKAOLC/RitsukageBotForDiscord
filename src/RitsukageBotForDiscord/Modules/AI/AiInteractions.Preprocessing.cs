c4101a4312891437193e78307b0a4b0522c23cf5
OLC <utsumabo@ritsukage.com>
2025-08-14 10:28:05 +0800

    improve info

--- src/RitsukageBotForDiscord/Modules/AI/AiInteractions.Preprocessing.cs
+++ src/RitsukageBotForDiscord/Modules/AI/AiInteractions.Preprocessing.cs
@@ -16,7 +16,7 @@ namespace RitsukageBot.Modules.AI
     // ReSharper disable once MismatchedFileName
     public partial class AiInteractions
     {
-        private async Task<string> TryPreprocessingMessage(string message,
+        private async Task<PreprocessingActionData[]> TryPreprocessingMessage(string message,
             CancellationToken cancellationToken = default)
         {
             try
@@ -25,7 +25,7 @@ namespace RitsukageBot.Modules.AI
                         out var chatClient))
                 {
                     Logger.LogWarning("Unable to get the assistant for preprocessing");
-                    return string.Empty;
+                    return [];
                 }
 
                 if (await ChatClientProvider.BuildUserChatMessage(Context.User.Username, Context.User.Id,
@@ -33,7 +33,7 @@ namespace RitsukageBot.Modules.AI
                     is not { } userMessage)
                 {
                     Logger.LogWarning("Unable to build user chat message for preprocessing");
-                    return string.Empty;
+                    return [];
                 }
 
                 var messageList = new List<ChatMessage>
@@ -54,7 +54,7 @@ namespace RitsukageBot.Modules.AI
                 if (!string.IsNullOrWhiteSpace(thinkContent))
                     Logger.LogInformation("Think content: {ThinkContent}", thinkContent);
                 if (hasJsonHeader) return await ProgressPreprocessingActions(jsonHeader!).ConfigureAwait(false);
-                return string.Empty;
+                return [];
             }
             catch (OperationCanceledException)
             {
@@ -65,13 +65,13 @@ namespace RitsukageBot.Modules.AI
                 Logger.LogError(e, "Error while preprocessing the message");
             }
 
-            return string.Empty;
+            return [];
         }
 
-        private async Task<string> ProgressPreprocessingActions(string json)
+        private async Task<PreprocessingActionData[]> ProgressPreprocessingActions(string json)
         {
-            if (string.IsNullOrWhiteSpace(json)) return string.Empty;
-            var result = new List<string>();
+            if (string.IsNullOrWhiteSpace(json)) return [];
+            var result = new List<PreprocessingActionData>();
             try
             {
                 var actionArrayData = JArray.Parse(json);
@@ -88,7 +88,7 @@ namespace RitsukageBot.Modules.AI
                             continue;
                         }
 
-                        var resultMessage = actionType switch
+                        var resultData = actionType switch
                         {
                             "web_search" => await PreprocessingWebSearch(data).ConfigureAwait(false),
                             "date_base_info" => await PreprocessingDateBaseInfo(data).ConfigureAwait(false),
@@ -99,11 +99,11 @@ namespace RitsukageBot.Modules.AI
                             "weather_now" => await PreprocessingQWeatherQueryWeatherNow(data).ConfigureAwait(false),
                             "weather_forecast" => await PreprocessingQWeatherQueryWeatherForecast(data)
                                 .ConfigureAwait(false),
-                            _ => string.Empty,
+                            _ => null,
                         };
 
-                        if (!string.IsNullOrEmpty(resultMessage))
-                            result.Add(resultMessage);
+                        if (resultData is not null)
+                            result.Add(resultData);
                     }
                     catch (Exception ex)
                     {
@@ -119,10 +119,10 @@ namespace RitsukageBot.Modules.AI
                 errorEmbed.WithDescription("An error occurred while processing the response");
             }
 
-            return string.Join("\n\n", result);
+            return [.. result];
         }
 
-        private async Task<string> PreprocessingWebSearch(JObject? data)
+        private async Task<PreprocessingActionData> PreprocessingWebSearch(JObject? data)
         {
             if (data is null || !data.TryGetValue("param", out var paramValue) || paramValue is not JObject paramToken)
                 throw new InvalidDataException("Invalid JSON data for web search action");
@@ -132,10 +132,11 @@ namespace RitsukageBot.Modules.AI
                 throw new InvalidDataException("Invalid query for web search action");
             var result = await GoogleSearchProviderService.WebSearch(param.Query).ConfigureAwait(false);
             var resultStrings = result.Take(5).Select(x => $"# {x.Title}\n{x.Snippet}\n\n{x.Link}");
-            return $"[Google Search: \"{param.Query}\"]\n{string.Join("\n\n", resultStrings)}";
+            return new($"Web Search: {param.Query}",
+                $"[Google Search: \"{param.Query}\"]\n{string.Join("\n\n", resultStrings)}");
         }
 
-        private static async Task<string> PreprocessingDateBaseInfo(JObject? data)
+        private static async Task<PreprocessingActionData> PreprocessingDateBaseInfo(JObject? data)
         {
             if (data is null || !data.TryGetValue("param", out var paramValue) || paramValue is not JObject paramToken)
                 throw new InvalidDataException("Invalid JSON data for date base info action");
@@ -165,10 +166,11 @@ namespace RitsukageBot.Modules.AI
                    忌：{today.Avoid}
                    """
                 : "未找到相关信息";
-            return $"[Date Base Info: {param.Date:yyyy-MM-dd}]\n{todayString}";
+            return new($"Get Date Info: {param.Date:yyyy-MM-dd}",
+                $"[Date Base Info: {param.Date:yyyy-MM-dd}]\n{todayString}");
         }
 
-        private static async Task<string> PreprocessingRangeDateBaseInfo(JObject data)
+        private static async Task<PreprocessingActionData> PreprocessingRangeDateBaseInfo(JObject data)
         {
             if (data is null) throw new InvalidDataException("Invalid JSON data for range date base info action");
             if (!data.TryGetValue("param", out var paramValue) || paramValue is not JObject paramToken)
@@ -212,10 +214,10 @@ namespace RitsukageBot.Modules.AI
 
             sb.AppendLine("---");
             sb.Append($"共计{days.Count}天");
-            return sb.ToString();
+            return new($"Get Date Info: {from:yyyy-MM-dd} ~ {end:yyyy-MM-dd}", sb.ToString());
         }
 
-        private async Task<string> PreprocessingBilibiliVideoInfo(JObject? data)
+        private async Task<PreprocessingActionData> PreprocessingBilibiliVideoInfo(JObject? data)
         {
             if (data is null || !data.TryGetValue("param", out var paramValue) || paramValue is not JObject paramToken)
                 throw new InvalidDataException("Invalid JSON data for bilibili video info action");
@@ -224,10 +226,11 @@ namespace RitsukageBot.Modules.AI
             var playerService = BiliKernelProviderService.GetRequiredService<IPlayerService>();
             var info = await playerService.GetVideoPageDetailAsync(new(param.Id.ToString(), null, null))
                 .ConfigureAwait(false);
-            return $"[Bilibili Video Info: {param.Id}]\n{InformationStringBuilder.BuildVideoInfo(info)}";
+            return new($"Get Bilibili Video Info: {param.Id}",
+                $"[Bilibili Video Info: {param.Id}]\n{InformationStringBuilder.BuildVideoInfo(info)}");
         }
 
-        private async Task<string> PreprocessingBilibiliUserInfo(JObject? data)
+        private async Task<PreprocessingActionData> PreprocessingBilibiliUserInfo(JObject? data)
         {
             if (data is null || !data.TryGetValue("param", out var paramValue) || paramValue is not JObject paramToken)
                 throw new InvalidDataException("Invalid JSON data for bilibili user info action");
@@ -236,10 +239,11 @@ namespace RitsukageBot.Modules.AI
             var userService = BiliKernelProviderService.GetRequiredService<IUserService>();
             var info = await userService.GetUserInformationAsync(param.Id.ToString())
                 .ConfigureAwait(false);
-            return $"[Bilibili User Info: {param.Id}]\n{InformationStringBuilder.BuildUserInfo(info)}";
+            return new($"Get Bilibili User Info: {param.Id}",
+                $"[Bilibili User Info: {param.Id}]\n{InformationStringBuilder.BuildUserInfo(info)}");
         }
 
-        private async Task<string> PreprocessingBilibiliLiveInfo(JObject? data)
+        private async Task<PreprocessingActionData> PreprocessingBilibiliLiveInfo(JObject? data)
         {
             if (data is null || !data.TryGetValue("param", out var paramValue) || paramValue is not JObject paramToken)
                 throw new InvalidDataException("Invalid JSON data for bilibili live info action");
@@ -248,10 +252,11 @@ namespace RitsukageBot.Modules.AI
             var liveService = BiliKernelProviderService.GetRequiredService<IPlayerService>();
             var info = await liveService.GetLivePageDetailAsync(new(param.Id.ToString(), null, null))
                 .ConfigureAwait(false);
-            return $"[Bilibili Live Info: {param.Id}]\n{InformationStringBuilder.BuildLiveInfo(info)}";
+            return new($"Get Bilibili Live Info: {param.Id}",
+                $"[Bilibili Live Info: {param.Id}]\n{InformationStringBuilder.BuildLiveInfo(info)}");
         }
 
-        private async Task<string> PreprocessingQWeatherQueryWeatherNow(JObject? data)
+        private async Task<PreprocessingActionData> PreprocessingQWeatherQueryWeatherNow(JObject? data)
         {
             if (data is null || !data.TryGetValue("param", out var paramValue) || paramValue is not JObject paramToken)
                 throw new InvalidDataException("Invalid JSON data for QWeather query weather now action");
@@ -279,10 +284,10 @@ namespace RitsukageBot.Modules.AI
             sb.AppendLine($"相对湿度：{weather.Humidity}%");
             sb.AppendLine($"气压：{weather.Pressure} hPa");
             sb.AppendLine($"能见度：{weather.Vis} km");
-            return sb.ToString();
+            return new($"Get Weather Now: {param.Location}", sb.ToString());
         }
 
-        private async Task<string> PreprocessingQWeatherQueryWeatherForecast(JObject? data)
+        private async Task<PreprocessingActionData> PreprocessingQWeatherQueryWeatherForecast(JObject? data)
         {
             if (data is null || !data.TryGetValue("param", out var paramValue) || paramValue is not JObject paramToken)
                 throw new InvalidDataException("Invalid JSON data for QWeather query weather forecast action");
@@ -313,8 +318,10 @@ namespace RitsukageBot.Modules.AI
             foreach (var hour in list)
                 sb.AppendLine(
                     $"{hour.FxTime:yyyy-MM-dd HH:mm} | {hour.Text} | {hour.Temp}°C | {hour.WindSpeed} km/h | {hour.WindDir} | {hour.WindScale}");
-            return sb.ToString();
+            return new($"Get Weather Forecast: {param.Location}", sb.ToString());
         }
+
+        private record PreprocessingActionData(string Action, string Result);
 
         private static class PreprocessingActionParam
         {
